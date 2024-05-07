2b61e5ce48804d58ddbc37a40c7c1ee8bc4b3741
Artem Kuzmin (DevExpress) <artem.kuzmin@devexpress.com>
2024-05-07 21:14:39 +0400

    SiteSection navigation url field is added

--- main/models.py
+++ main/models.py
@@ -5,3 +5,4 @@ class SiteSection(models.Model):
     id = models.IntegerField(primary_key=True)
     display_text = models.CharField(max_length=30)
     icon_url = models.CharField(max_length=100)
+    navigation_url = models.CharField(max_length=100, default='#')
