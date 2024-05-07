e3faacefe0fd6248af9ec64780a3f7a6eb42a9a1
Artem Kuzmin (DevExpress) <artem.kuzmin@devexpress.com>
2024-05-07 21:09:23 +0400

    Layout navbar is bound to data

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SiteSection
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = { 'site_sections' :  SiteSection.objects.all() }
+    return render(request, 'index.html', context=context)
