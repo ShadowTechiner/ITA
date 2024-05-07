1d36ad641e0794eb0563a01c94b9f4a1c0f1eb98
Artem Kuzmin (DevExpress) <artem.kuzmin@devexpress.com>
2024-05-07 20:51:20 +0400

    SiteSection data class is created; Admin user is created;

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class SiteSection(models.Model):
+    id = models.IntegerField(primary_key=True)
+    display_text = models.CharField(max_length=30)
+    icon_url = models.CharField(max_length=100)
