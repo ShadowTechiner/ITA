1eb6dc7a7dc886b18b8e7c7f65df0789b4665b05
Artem Kuzmin (DevExpress) <artem.kuzmin@devexpress.com>
2024-05-18 11:29:03 +0400

    DataTicketTableView added

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
-from .models import SiteSection
+from .models import SiteSection, SidebarSection
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -12,3 +12,13 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["site_sections"] = SiteSection.objects.all()
         return context
+
+class DataTicketTableView(TemplateView):
+
+    template_name = ''
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['site_sections'] = SiteSection.objects.all()
+        context['sidebar_sections'] = SidebarSection.objects.all()
+        
