bc355217d21c99b9bbca479e4a8fb23b02c775a6
Artem Kuzmin (DevExpress) <artem.kuzmin@devexpress.com>
2024-05-18 16:37:20 +0400

    Ticket model migrations applied

--- main/models.py
+++ main/models.py
@@ -39,17 +39,29 @@ class Product(models.Model):
     id = models.IntegerField(primary_key=True, verbose_name='Product ID')
     display_text = models.CharField(max_length=500, verbose_name='Product Name')
 
+    def __str__(self):
+        return self.display_text
+
 class Build(models.Model):
     id = models.IntegerField(primary_key=True, verbose_name='Build ID')
     display_text = models.CharField(max_length=10, verbose_name='Version')
+
+    def __str__(self):
+        return self.display_text
 
 class OperationalSystem(models.Model):
     id = models.IntegerField(primary_key=True, verbose_name='ID')
     display_text = models.CharField(max_length=100, verbose_name='OS Name')
 
+    def __str__(self):
+        return self.display_text
+
 class IDE(models.Model):
     id = models.IntegerField(primary_key=True, verbose_name='ID')
     display_text = models.CharField(max_length=100, verbose_name='IDE Name')
+
+    def __str__(self):
+        return self.display_text
 
 class Ticket(models.Model):
     ticket_number = models.CharField(primary_key=True, max_length=20, verbose_name='Ticket Number')
